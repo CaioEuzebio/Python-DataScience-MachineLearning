f17b9d751cb4ce5818d5d621b890e27d691d2632
Caio Euzébio <39926679+CaioEuzebio@users.noreply.github.com>
2019-11-05 15:46:23 -0300

    Update LogisticsOrderClassification.py

--- LogisticsOrderClassification.py
+++ LogisticsOrderClassification.py
@@ -6,7 +6,13 @@ import numpy as np
 
 df1 = pd.read_csv('lines.csv', encoding='latin-1', low_memory=False)
 
+df6 = pd.read_csv('lines.csv', encoding='latin-1', low_memory=False)
+
+df15 = df6.groupby(['Order No']).last()
+df16 = df6.groupby(['Order No']).sum()
+
 df1.index=df1['Order No']
+
 df3 = df1.assign(col=df1.groupby(level=0).PartNo.cumcount()).pivot(columns='col', values='PartNo')
 
 df3['ListProds'] = df3.values.tolist()
@@ -21,4 +27,16 @@ df3['ListProds'] = df3['ListProds'].apply(lambda x: np.sort(np.unique(x)))
 df4 = df3[['ListProds']].copy()
 df4['QtyPNs'] = df3['QtyProds'] = df3.ListProds.apply(lambda x: len(x))
 
-df4.to_csv('Gerar.csv')
+df4.reset_index(inplace=True)
+df15.reset_index(inplace=True)
+df16.reset_index(inplace=True)
+
+df67 = pd.merge(df4, df15[['Order No','PGI`d']], on='Order No', how='right')
+df67 = pd.merge(df67, df15[['Order No','Order Type']], on='Order No', how='right')
+df67 = pd.merge(df67, df15[['Order No','Cut Off Date']], on='Order No', how='right')
+df67 = pd.merge(df67, df16[['Order No','Qty']], on='Order No', how='right')
+df67 = pd.merge(df67, df15[['Order No','transport mode']], on='Order No', how='right')
+df67 = pd.merge(df67, df15[['Order No','Carrier']], on='Order No', how='right')
+df67 = pd.merge(df67, df15[['Order No','Carrier Sort']], on='Order No', how='right')
+
+df67.to_csv('Gerar DNs.csf', index=False)
