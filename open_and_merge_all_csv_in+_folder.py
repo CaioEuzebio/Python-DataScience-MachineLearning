e56bb8c98799b0c67a7f75aa69dcf14ae1e5a7a0
Caio Euzébio <caio.145@hotmail.com>
2021-11-29 15:19:40 -0300

    Update open_and_merge_all_csv_in+_folder.py

--- open_and_merge_all_csv_in+_folder.py
+++ open_and_merge_all_csv_in+_folder.py
@@ -1,3 +1,8 @@
 import glob
+import pandas
 
-df = pd.concat(map(pd.read_csv, glob.glob('data/*.csv')))
+path = r'path'                    
+all_files = glob.glob(os.path.join(path, "*.csv"))     
+
+df_from_each_file = (pd.read_csv(f, sep=';') for f in all_files)
+concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
